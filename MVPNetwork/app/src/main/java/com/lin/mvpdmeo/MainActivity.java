75e5f8b9567606e2df4a918e7abd8e8b9f58d201
lingjianzhong <lingjianzhong@limin-mac-mini.local>
2019-10-23 18:43:35 +0800

    ----

--- MVPNetwork/app/src/main/java/com/lin/mvpdmeo/MainActivity.java
+++ MVPNetwork/app/src/main/java/com/lin/mvpdmeo/MainActivity.java
@@ -2,13 +2,53 @@ package com.lin.mvpdmeo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.lin.mvpdmeo.mvp.base.BaseMVPActivity;
+import com.lin.mvpdmeo.mvp.base.BaseView;
+import com.lin.mvpdmeo.mvp.presenter.MainPresenter;
+
+public class MainActivity extends BaseMVPActivity<MainPresenter> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected MainPresenter createPresenter() {
+        return new MainPresenter();
+    }
+
+    @Override
+    public void showLoading() {
+
+    }
+
+    @Override
+    public void dismissLoading() {
+
+    }
+
+    @Override
+    public void onEmpty(Object tag) {
+
+    }
+
+    @Override
+    public void onError(Object tag, String errorMsg) {
+
+    }
+
+    @Override
+    public void showResult(Object bean) {
+
+    }
+
+    @Override
+    public Context getContext() {
+        return null;
     }
 }
---
+++ MVPNetwork/app/src/main/java/com/lin/mvpdmeo/mvp/presenter/MainPresenter.java
@@ -0,0 +1,13 @@
+package com.lin.mvpdmeo.mvp.presenter;
+
+import com.lin.mvpdmeo.MainActivity;
+import com.lin.mvpdmeo.mvp.base.BasePresenter;
+
+public class MainPresenter extends BasePresenter<MainActivity> {
+    /**
+     * 登录
+     */
+    public void login(String username, String password){
+
+    }
+}
